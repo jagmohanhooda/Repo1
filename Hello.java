2059ff7c9f979398e226c9298ffa910cc295c2f9
jagmohanhooda <jagmohanhooda@hotmail.com>
2020-02-27 18:36:44 -0500

    update to new

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello{
 	public static void main(String[] args){
 
         	for(int i=1;i<=6;i++){
-			System.out.println("Hello World of AWS = "+i);
+			System.out.println("Hello to this new World of AWS = "+i);
 		}
 	}
 
